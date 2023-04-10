5834b721023ef1730f2d0226023e37a02a5bb8e3
Pranay Mangal <mangal.pranay@gmail.com>
2023-04-10 17:28:28 -0400

    axios, context, backend conn.

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,38 +1,83 @@
 import React from 'react'
-import {useState,useEffect} from 'react'
+import {useState,useEffect,useRef} from 'react'
+import {authContext} from '../context/AuthContext'
 const Login = () => {
+    const {auth,setAuth} =useContext(authContext);
+    
     const [name,setName] = useState('')
     const [password,setPassword] = useState('')
     const [success,setSuccess] = useState(false)
     const [errmessage,setErrmessage] = useState('')
 
+    const userRef = useRef();
+    const errRef = useRef();
+
+    const LOGIN_URL = '/auth'
+
     useEffect(() => {
-      
+      userRef.current.focus();
      }, [])
     
+     useEffect(() => {
+        
+        setErrmessage('')
+     }, [name,password])
+     
+
     const handleSubmit = (e)=>{
     e.preventDefault();
-    console.log(name,password)
+   try {
+    const response = axios.post(LOGIN_URL, 
+       JSON.stringify({name,password})
+        ,
+        {
+            headers:{'Content-Type':'application/json'}
+        })
+        console.log(JSON.stringify(response?.data));
+        const accessToken = response?.data?.accessToken;
+        const roles = response?.data?.roles;
+        setAuth({name,password,accessToken,roles})
+   } catch (err) {
+        // if(!err?.response){
+        //     setErrmessage('no response received fro server');
+        // }elseif( err.response?.status === 400) {
+
+        // setErrmessage("user or password missing");
+        // }elseif(err.response?.status===401){
+        //     setErrmessage("unauthorized access");
+        // }else{
+        //     setErrmessage('login failed');
+
+           
+        // }
+        errRef.current.focus();
+   }
+
     setName('');
     setPassword('')
-
+        setSuccess(true);
     }
   return (
-    <> (success? (
+    <>
+    { success? (
         <div>
         <h1>form submitted successfully</h1>
+        <p><a href="#">Go to home</a></p>
         </div>
         
     ) : 
-        (<div>
-        <form action="" onSubmit={handleSubmit}>
+        (<section>
+            <p ref={errRef} className={errmessage? "errmsg": "offscreen"  }>{errmessage}</p>
+
+        <form  onSubmit={handleSubmit}>
             <label htmlFor="name"></label>
             <input type="text"
+                ref={userRef}
                    id="name"
                    required
                    value={name}
                    onChange={e=> setName(e.target.value)}
-
+                    autoComplete= 'false'
             />
 
               <label htmlFor="password"></label>
@@ -43,10 +88,17 @@ const Login = () => {
                    onChange={e=> setPassword(e.target.value)}
 
             />
+
+            <button>Sign in</button>
         </form>
 
-    </div>)
-    )
+        <p>
+            {/* Router link goes here*/}
+            <a href="#">Need and accohnt , sign up!</a>
+        </p>
+
+    </section>)
+    }
     </>
   )
 }
